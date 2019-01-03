e7f3e6f27952f471eb42a0c345912c960e8ba94a
Cody Rickman <cody@creativemindstudios.com>
2019-01-03 18:43:13 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,20 @@
 require 'pry'
 
 class String
-
   def sentence?
-    
+    return self.end_with?(".")
   end
 
   def question?
-
+    return self.end_with?("?")
   end
 
   def exclamation?
-
+    return self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(".").split("?").split("!")
+    return self.count
   end
 end
